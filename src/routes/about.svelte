0941fb6fa97a6bc3a96681a07fd83b7767770589
Slatvinskyi <andrii.slatvinskyi@gmail.com>
2021-06-29 14:10:26 +0300

    first commit

---
+++ src/routes/about.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+    <title>About Pokedex</title>
+</svelte:head>
+<h1>About</h1>
+
+<p>Pokemon App</p>
---
+++ src/routes/index.svelte
@@ -0,0 +1,17 @@
+<script>
+import {pokemon} from "../stores/pokestore";
+console.log($pokemon)
+</script>
+<svelte:head>
+
+    <title>Pokedex</title>
+
+</svelte:head>
+<h1>SvelteKit Pokedex</h1>
+
+{#each $pokemon as pokeman}
+<p>{pokeman.name}</p>
+{/each}
+
+
+
